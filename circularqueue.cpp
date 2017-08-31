920d9f1c1cadc2c2b052dd57cc688a190beeb322
Dan Dees <dand@appsmiths.com>
2017-09-01 06:31:38 +0700

    Make it thread-safe.

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <pthread.h>
 using namespace std;
 
 
@@ -9,40 +10,79 @@ class Queue
     int queuesize;
     int * A;
     int front, rear;
+    pthread_mutex_t mp = PTHREAD_MUTEX_INITIALIZER;
 
   public:
     // Constructor - set front and rear as -1
     Queue() {
-      queuesize = 3;
-      A = new int[queuesize];
+      queuesize = -1;
+      A = NULL;
       front = -1;
       rear = -1;
+      pthread_mutex_init(&mp,NULL);
     }
 
     ~Queue() {
+      pthread_mutex_lock(&mp);
       cout << "Calling deconstructor" << endl;
-      delete [] A;
+      if (A!=NULL) {
+        delete [] A;
+        A = NULL;
+      }
+      pthread_mutex_unlock(&mp);
+      pthread_mutex_destroy(&mp);
+    }
+
+    void Initialize(int inputsize) {
+      pthread_mutex_lock(&mp);
+      if (inputsize>0) {
+        queuesize = inputsize;
+        A = new int[queuesize];
+        front = -1;
+        rear = -1;
+      } else {
+        cout << "Error: queue size must be positive!!!" << endl;
+        pthread_mutex_unlock(&mp);
+        return;
+      }
+      pthread_mutex_unlock(&mp);
     }
 
     // To check whether Queue is empty or not
     bool IsEmpty()
     {
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        return true;
+      }
       return (front == -1 && rear == -1);
     }
 
     // To check whether Queue is full or not
     bool IsFull()
     {
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        return false;
+      }
       return (rear+1)%queuesize == front ? true: false;
     }
 
     // Inserts an element in queue at rear end
     void Enqueue(int x)
     {
+      pthread_mutex_lock(&mp);
       cout << "Enqueuing " << x << " \n";
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        pthread_mutex_unlock(&mp);
+        return;
+      }
+
       if (IsFull())
       {
         cout << "Error: Queue is Full\n";
+        pthread_mutex_unlock(&mp);
         return;
       }
 
@@ -54,22 +94,37 @@ class Queue
       }
 
       A[rear] = x;
+      pthread_mutex_unlock(&mp);
     }
 
     void Dequeue() {
+      pthread_mutex_lock(&mp);
       cout << "Dequeuing \n";
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        pthread_mutex_unlock(&mp);
+        return;
+      }
+
       if (IsEmpty()) {
         cout << "Error: Queue is Empty\n";
+        pthread_mutex_unlock(&mp);
         return;
       } else if (front == rear) {
         rear = front = -1;
       } else {
         front = (front + 1)%queuesize;
       }
+      pthread_mutex_unlock(&mp);
     }
 
     int Front()
     {
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        return -1;
+      }
+
       if (front == -1)
       {
         cout << "Error: cannot return front from a empty queue\n";
@@ -80,6 +135,11 @@ class Queue
 
     void Print()
 	  {
+      if (queuesize<=0) {
+        cout << "Must init queue first!!!" << endl;
+        return;
+      }
+
 		  // Finding number of elements in queue
 		  int count = (rear+queuesize-front)%queuesize + 1;
 		  cout<<"Queue: ";
@@ -94,6 +154,7 @@ class Queue
 
 int main() {
   Queue Q;
+  Q.Initialize(3);
   Q.Enqueue(2); Q.Print();
   Q.Enqueue(4); Q.Print();
   Q.Enqueue(6); Q.Print();
